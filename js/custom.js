8f49b929a9bad8738b23000325950fea4775846b
_kaikc <jankyle@gmail.com>
2024-03-05 14:58:10 +0800

    @240305

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $(function () {
 	$("#header").load("header.html .navbar");
 	$("#kv").load("kv.html .container");
+	$("#castview").load("castview.html .container");
 	$("#footer").load("footer.html .container");
 });
